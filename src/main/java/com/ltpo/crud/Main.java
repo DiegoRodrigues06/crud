c61121ba75e6870110e9c51c3ae734cc31847344
giovanniamorimprof <giovanni.amorim.prof@gmail.com>
2025-02-23 18:54:01 -0300

    refatoracao da estrutura

--- src/main/java/com/ltpo/crud/Main.java
+++ src/main/java/com/ltpo/crud/Main.java
@@ -1,60 +1,39 @@
 package com.ltpo.crud;
 
-import com.ltpo.crud.dao.UsuarioDAO;
-import com.ltpo.crud.model.Usuario;
-
-import java.util.List;
-import java.util.Scanner;
+import com.ltpo.crud.service.UsuarioService;
+import com.ltpo.crud.ui.Menu;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        Menu menu = new Menu();
+        UsuarioService usuarioService = new UsuarioService();
         int opcao;
 
         do {
-            System.out.println("\n### Sistema de Gerenciamento de Usuários ###");
-            System.out.println("1. Cadastrar usuário");
-            System.out.println("2. Listar usuários");
-            System.out.println("3. Atualizar usuário");
-            System.out.println("4. Deletar usuário");
-            System.out.println("5. Sair");
-            System.out.print("Escolha uma opção: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine();
-
+            opcao = menu.exibirMenu();
             switch (opcao) {
                 case 1:
-                    System.out.print("Nome: ");
-                    String nome = scanner.nextLine();
-                    System.out.print("Email: ");
-                    String email = scanner.nextLine();
-                    usuarioDAO.criarUsuario(new Usuario(0, nome, email));
-                    System.out.println("Usuário cadastrado com sucesso!");
+                    usuarioService.cadastrarUsuario(
+                            menu.lerString("Nome: "),
+                            menu.lerString("Email: ")
+                    );
                     break;
                 case 2:
-                    List<Usuario> usuarios = usuarioDAO.listarUsuarios();
-                    System.out.println("\nLista de Usuários:");
-                    for (Usuario u : usuarios) {
-                        System.out.println("ID: " + u.getId() + " | Nome: " + u.getNome() + " | Email: " + u.getEmail());
-                    }
+                    usuarioService.listarUsuarios();
                     break;
                 case 3:
-                    System.out.print("ID do usuário a atualizar: ");
-                    int idAtualizar = scanner.nextInt();
-                    scanner.nextLine();
-                    System.out.print("Novo nome: ");
-                    String novoNome = scanner.nextLine();
-                    System.out.print("Novo email: ");
-                    String novoEmail = scanner.nextLine();
-                    usuarioDAO.atualizarUsuario(new Usuario(idAtualizar, novoNome, novoEmail));
-                    System.out.println("Usuário atualizado com sucesso!");
+                    int idAtualizar = Integer.parseInt(menu.lerString("ID do usuário a atualizar: "));
+                    menu.lerString(""); // Limpa o buffer após a leitura do id
+                    usuarioService.atualizarUsuario(
+                            idAtualizar,
+                            menu.lerString("Novo nome: "),
+                            menu.lerString("Novo email: ") // O email agora é lido corretamente
+                    );
                     break;
                 case 4:
-                    System.out.print("ID do usuário a deletar: ");
-                    int idDeletar = scanner.nextInt();
-                    usuarioDAO.deletarUsuario(idDeletar);
-                    System.out.println("Usuário deletado com sucesso!");
+                    int idDeletar = Integer.parseInt(menu.lerString("ID do usuário a deletar: "));
+                    menu.lerString(""); // Limpa o buffer após a leitura do id
+                    usuarioService.deletarUsuario(idDeletar);
                     break;
                 case 5:
                     System.out.println("Saindo do sistema...");
@@ -64,8 +43,6 @@ public class Main {
             }
         } while (opcao != 5);
 
-        scanner.close();
+        menu.fecharScanner();
     }
-
-
 }
